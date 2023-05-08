a0a7e30c46872420aedd6a039d4f166f523de441
Zein Hasyim <zeinhasyim111@gmail.com>
2023-05-08 23:25:07 +0700

    Commit Pertama

---
+++ src/scripts/sw.js
@@ -0,0 +1,38 @@
+import { precacheAndRoute } from 'workbox-precaching';
+
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', () => {
+  console.log('Service Worker: Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('push', () => {
+  console.log('Service Worker: Pushed');
+
+  const dataJson = event.data.Json();
+  const notification = {
+    title: dataJson.title,
+    options: {
+      body: dataJson.options.body,
+      icon: dataJson.options.icon,
+      image: dataJson.options.image,
+    },
+  };
+
+  // eslint-disable-next-line max-len
+  event.waitUntil(self.ServiceWorkerRegistration.showNotification(notification.title, notification.options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+
+  const chainPromise = async () => {
+    console.log('Notification has been clicked');
+    await self.clients.openWindow('https://www.dicoding.com/');
+  };
+
+  event.waitUntil(chainPromise());
+});
